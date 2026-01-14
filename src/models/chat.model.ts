09a5d7fdea888744834ab2005b68575adb465f4f
Karan Jamwal <karanjamwal2004@gmail.com>
2026-01-14 16:40:55 +0530

    chat and message models created

---
+++ src/models/chat.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema, model, Document } from "mongoose";
+
+interface ChatDocument extends Document {
+  participants: mongoose.Types.ObjectId[];
+  lastMessage: mongoose.Types.ObjectId;
+  isGroup: boolean;
+  groupName?: string;
+  createdBy: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const chatSchema = new Schema<ChatDocument>(
+  {
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    lastMessage: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Message",
+        default: null,
+      },
+    ],
+    isGroup: {
+      type: Boolean,
+      default: false,
+    },
+    groupName: {
+      type: String,
+      default: null,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ChatModel = model<ChatDocument>("Chat", chatSchema);
+
+export default ChatModel;
---
+++ src/models/message.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, model, Document } from "mongoose";
+
+interface MessageDocument extends Document {
+  chatId: mongoose.Types.ObjectId;
+  sender: mongoose.Types.ObjectId;
+  content: string;
+  image?: string;
+  replyTo?: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const messageSchema = new Schema<MessageDocument>(
+  {
+    chatId: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: { type: String },
+    image: { type: String },
+    replyTo: {
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const MessageModel = model<MessageDocument>("Message", messageSchema);
+
+export default MessageModel;
