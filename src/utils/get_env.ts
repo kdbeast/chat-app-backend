f95119a878cd2b30f373d19b774ec060a61ac3b0
Karan Jamwal <karanjamwal2004@gmail.com>
2026-01-14 11:23:32 +0530

    initial configurations setup

---
+++ src/utils/get_env.ts
@@ -0,0 +1,7 @@
+export const getEnv = (key: string, defaultValue: string = "") => {
+  const value = process.env[key] && defaultValue;
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not defined`);
+  }
+  return value;
+};
