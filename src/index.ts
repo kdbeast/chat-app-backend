9f4c0ea394f5f45d9e639e7306d2f711bd3b0902
Karan Jamwal <karanjamwal2004@gmail.com>
2026-01-20 13:25:28 +0530

    feat: Configure production static file serving and relax message content validation to allow empty strings.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import path from "path";
 import routes from "./routes";
 import passport from "passport";
+import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 import { Env } from "./config/env.config";
 import { initializeSocket } from "./lib/socket";
@@ -11,6 +13,9 @@ import express, { Request, Response } from "express";
 import connectToDatabase from "./config/database.config";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 
@@ -31,15 +36,27 @@ app.get(
     res
       .status(HTTPSTATUS.OK)
       .json({ message: "Server is healthy", status: "OK" });
-  })
+  }),
 );
 
 app.use("/api", routes);
+
+if (Env.NODE_ENV === "production") {
+  const clientPath = path.resolve(__dirname, "../../frontend/dist");
+
+  // Serve static files
+  app.use(express.static(clientPath));
+
+  app.get(/^(?!\/api).*/, (req: Request, res: Response) => {
+    res.sendFile(path.join(clientPath, "index.html"));
+  });
+}
+
 app.use(errorHandler);
 
 server.listen(PORT, async () => {
   console.log(
-    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`,
   );
   await connectToDatabase();
 });
--- src/validators/message.validator.ts
+++ src/validators/message.validator.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const sendMessageSchema = z
   .object({
     chatId: z.string().trim().min(1),
-    content: z.string().trim().min(1).optional(),
+    content: z.string().trim().optional(),
     image: z.string().trim().optional(),
     replyToId: z.string().trim().optional(),
   })
